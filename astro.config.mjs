32ad8312613ffa4ed95f998e193995e4c1bed8fc
level1rogue <abc@MacBook-Air.local>
2025-11-26 14:19:43 +0100

    chore: Update astro.config.mjs for configuration consistency; add deploy.yml for GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
 
-import svelte from '@astrojs/svelte';
+import svelte from "@astrojs/svelte"
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte()]
-});
+	integrations: [svelte()],
+	site: "https://level1rogue.github.io",
+})
